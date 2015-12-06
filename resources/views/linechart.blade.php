6d27fcc54091031efbf066b16454c4c0d1183fd8
Corbett Weinberg <corbettweinberg@gmail.com>
2015-12-05 20:46:14 -0500

    changed php short tag to regular tag in linechart.blade.php

--- resources/views/linechart.blade.php
+++ resources/views/linechart.blade.php
@@ -13,18 +13,18 @@
 
 @section('ChartUno')
     
-<? echo \Lava::render('PieChart', 'myPieChart', 'chart-div') ?>
+<?php echo \Lava::render('PieChart', 'myPieChart', 'chart-div') ?>
 
 @stop
 
 @section('ChartDos')
     
-<? echo \Lava::render('PieChart', 'myThreeDPieChart', 'chart2-div') ?>
+<?php echo \Lava::render('PieChart', 'myThreeDPieChart', 'chart2-div') ?>
 
 @stop
 
 @section('ChartTres')
     
-<? echo \Lava::render('DonutChart', 'myDonut', 'chart3-div') ?>
+<?php echo \Lava::render('DonutChart', 'myDonut', 'chart3-div') ?>
 
 @stop
